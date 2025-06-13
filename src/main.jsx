5c9c24d2ca291597e124d430807b57fc8cca6f35
tomlukewarden <tomlukeprogramming@outlook.com>
2025-06-13 13:02:38 +0100

    refactor: wrap App component in StrictMode for improved development checks

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
-
+import { StrictMode } from 'react'
 import { HashRouter } from 'react-router-dom';
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-<HashRouter>
-  <App />
-</HashRouter>
+  <StrictMode>
+    <App />
+  </StrictMode>,
 )
