34fcffbce0f13213520d68ce5b670c667de9e4f1
tomlukewarden <tomlukeprogramming@outlook.com>
2025-06-13 09:24:50 +0100

    ...just didnt import the project css

--- src/pages/components/projectCard.jsx
+++ src/pages/components/projectCard.jsx
@@ -1,10 +1,22 @@
 
 function ProjectCard({ project }) {
-  project = project || {
+  project = [{
     name: "Project Name",
     description: "Project Description",
     link: "https://example.com",
+    github: ""},
+  {
+    name: "Project Name 2",
+    description: "Project Description 2",
+    link: "https://example2.com",
+    github: ""},
+  {
+    name: "Project Name 3",
+    description: "Project Description 3",
+    link: "https://example3.com",
     github: ""}
+  ]
+  
   return (
     <div className="project-card mt-8 card">
       <div className="card-body">
--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -2,6 +2,7 @@ import Header from "./components/header";
 import Footer from "./components/footer";
 import Bubbles from "./components/bubbles";
 import ProjectCard from "./components/projectCard";
+import "./styles/project.css";
 function Projects () {
   return (
     <div className="project-page-wrapper">
