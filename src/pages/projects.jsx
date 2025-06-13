51f6841c8cfebd9bcbf537c8254d3616acb563a5
tomlukewarden <tomlukeprogramming@outlook.com>
2025-06-13 08:19:47 +0100

    feat: update Projects page layout and content structure

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -4,9 +4,15 @@ function Projects () {
     <div className="flex flex-col items-center justify-center h-screen">
       <h1 className="text-4xl font-bold mb-4">Projects</h1>
       <p className="text-lg">My projects</p>
-      <ul>
-        <li>The Botanist</li>
-      </ul>
+        <div className="mt-8 card">
+            <div className="card-body">
+                <h2 className="card-title">Project 1</h2>
+                <p className="card-text">Description of project 1.</p>
+                <a href="">Link to Deployed Project</a>
+                <a href="">Github Link</a>
+                <a href="">More info</a>
+            </div>
+    </div>
     </div>
   );
 }
